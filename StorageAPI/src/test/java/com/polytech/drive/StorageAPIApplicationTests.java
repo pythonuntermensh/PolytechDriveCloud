4d21cc9a5210e33d246e9f1c49626e288345fd98
optional <pythonuntermensh@gmail.com>
2023-11-17 23:54:36 +0400

    Divided project into 2 modules: RestMVC and StorageAPI.

---
+++ StorageAPI/src/test/java/com/polytech/drive/StorageAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytech.drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
